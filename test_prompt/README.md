f98c98b13b33de4dd84306275b4a88337bedbdcd
Romain Linkov <rlinkov@shi-r11-p1.s19.be>
2021-04-26 18:07:10 +0200

    remove old dir

--- test_prompt/README.md
+++
@@ -1,5 +0,0 @@
-# minishell
-
-# Resources
-Starting point to learn how the whole process works
-- https://brennan.io/2015/01/16/write-a-shell-in-c/
--- test_prompt/minishell_wip/test_promp.sh
+++
@@ -1,6 +0,0 @@
-(cd ../libft/ && make)
-gcc -Wall -Werror -Wextra prompt.c ../libft/libft.a
-(cd ../libft/ && make fclean)
-clear
-echo "Prompt loop : CTRL+D OR CTRL+C to quit\n"
-./a.out
