e1d4343e93d003d9a51da6ca9e6f1903eb6119ad
Romain Linkov <rlinkov@shi-r11-p1.s19.be>
2021-05-12 17:16:37 +0200

    removed msh_parsing directory as it is not needed anymore

--- msh_parsing/minishell_parser/msh_launch.sh
+++
@@ -1,6 +0,0 @@
-(cd ../libft/ && make)
-gcc -Wall -Werror -Wextra minishell.c clean_cmd.c syntaxe_cmd.c error.c ft_split_msh.c ../libft/libft.a
-(cd ../libft/ && make fclean)
-clear
-echo "Prompt loop : CTRL+D OR CTRL+C to quit\n"
-./a.out
