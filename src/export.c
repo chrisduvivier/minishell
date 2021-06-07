485f158308d2158185b5183d5fbc916ec2c261cf
Romain Linkov <rlinkov@shi-r11-p1.s19.be>
2021-06-07 17:43:18 +0200

    functionning append with export

--- src/export.c
+++ src/export.c
@@ -6,7 +6,7 @@
 /*   By: rlinkov <rlinkov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/03 14:36:16 by rlinkov           #+#    #+#             */
-/*   Updated: 2021/06/07 16:42:16 by rlinkov          ###   ########.fr       */
+/*   Updated: 2021/06/07 17:42:24 by rlinkov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,7 +78,7 @@ int is_key_existing(char *key)
     int len;
 
     i = 0;
-    len = ft_strlen(key);
+    len = ft_strlen(key);    
     while (g_msh.env[i] && g_msh.env[i] != NULL)
     {
         if (ft_strncmp(key, g_msh.env[i], len) == 0)
@@ -101,6 +101,7 @@ void    add_var_to_env(char *key, char *value)
 {
     int env_size;
     char **new_env;
+
     if (is_key_existing(key))
         set_env_value(key, value);
     else
@@ -121,6 +122,41 @@ void    add_var_to_env(char *key, char *value)
         g_msh.env = new_env;
     }
 }
+
+/*
+** Append a value to the value of an existing environment var
+**/
+
+void    append_var_to_env(char *key, char *value)
+{
+    char *n_key;
+    char *n_value;
+    char *temp;
+    int  i;
+
+    n_key = malloc(ft_strlen(key) - 1);
+    ft_strlcpy(n_key, key, ft_strlen(key));
+    if (!is_key_existing(n_key))
+    {
+        free(n_key);
+        return (add_var_to_env(key, value));
+    }
+    temp = find_var(n_key);
+    i = 0;
+    while (i < (int)ft_strlen(temp))
+    {
+        if (temp[i] == SPACE)
+            temp[i] = ' ';
+        i++;
+    }
+    n_value = malloc(sizeof(char) * (ft_strlen(temp) + ft_strlen(value) + 1));
+    ft_strlcpy(n_value, temp, ft_strlen(temp) + 1);
+    ft_strlcat(n_value, value, ft_strlen(temp) + ft_strlen(value) + 1);
+    set_env_value(n_key, n_value);
+    free(n_key);
+    free(n_value);
+    free(temp);
+} 
 
 /*
 ** Check if the key is valid
@@ -184,16 +220,10 @@ void msh_bi_export(t_cmd_table t_cmd)
         i++;
     value = malloc(sizeof(char) * i); //proteger le malloc
     ft_strlcpy(value, t_cmd.argv[1] + pos_equal + 1, i);
-    printf("KEY : %s| VALUE : %s|\n", key, value);
     if (is_key_valid(key, (int)ft_strlen(key)) == 2)
-    {
-        return ;
-    }
+        append_var_to_env(key, value);
     else
-    {
         add_var_to_env(key, value);
-    }
-    
     free(key);
     free(value);
 }
