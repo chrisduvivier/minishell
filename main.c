9386baf978a9a70486146509d2cfdd5a29a9def3
Romain Linkov <rlinkov@shi-r11-p1.s19.be>
2021-06-09 14:36:15 +0200

    normv3 main.c

--- main.c
+++ main.c
@@ -6,23 +6,22 @@
 /*   By: cduvivie <cduvivie@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/15 17:50:45 by rlinkov           #+#    #+#             */
-/*   Updated: 2021/06/08 10:53:14 by cduvivie         ###   ########.fr       */
+/*   Updated: 2021/06/09 14:35:39 by rlinkov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 /*
-**	declaration of the only global variable
-*/
+ **	declaration of the only global variable
+ */
 
-t_msh g_msh;
+t_msh	g_msh;
 
 int	main(int argc, char **argv, char **envp)
 {
 	(void) argc;
 	(void) argv;
-
 	t_msh_init();
 	copy_env(envp);
 	prompt();
